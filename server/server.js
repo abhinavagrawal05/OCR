7b17509108e8e8622fd7c2c48ba1ed5b0134204f
abhinavagrawal05 <138150223+abhinavagrawal05@users.noreply.github.com>
2023-12-27 11:29:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const cors = require('cors');
+//const path = require('path');
 
 
 dotenv.config();
@@ -28,16 +29,14 @@ app.use(cors());
 app.use('/api/ocr', require('./routes/ocr'));
 
 //deploying config
+const path = require("path");
+__dirname = path.resolve();
 
-// const path = require("path");
-// __dirname = path.resolve();
-
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static(path.join(__dirname, "/client/build")));
-//     app.get("*", (req,res) => {
-//         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//     });
-// }
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
